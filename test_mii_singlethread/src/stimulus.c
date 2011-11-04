5815530a6b308c925202c7bb920ebb8ba77cf7a7
Henk Muller <henk@xmos.com>
2011-11-04 15:18:56 +0000

    First version with timestamps - not in AVB interface yet

--- test_mii_singlethread/src/stimulus.c
+++ test_mii_singlethread/src/stimulus.c
@@ -29,27 +29,26 @@ int allTriggersDone = 0;
 
 char triggerBefore[MT], triggerAfter[MT];
 
-int setTriggers() {
+int setTriggers(int step) {
     int i;
-    int step = 1;
     if (step != 1) {
         fprintf(stderr, "Warning only doing 1 in every %d steps for shmoo\n", step);
     }
     for(i = ltoh*8 + 96; i >= ltoh*8 - 64; i-=step) {  // Make tail of Tx collide with head of Rx
         triggerBefore[i] = 1;
-        triggerCnt++;
     }
     for(i = 128; i > 0; i-=step) {                    // Make head of Tx collide with head of Rx
         triggerBefore[i] = 1;
-        triggerCnt++;
     }
     for(i = 0; i <= 128; i+=step) {                   // And tail with tail
         triggerAfter[i] = 1;
-        triggerCnt++;
     }
     for(i = ltod*8 - 96; i <= ltoh*8 + 64 ; i+=step) { // and head with tail.
         triggerAfter[i] = 1;
-        triggerCnt++;
+    }
+    for(i = 0; i < MT; i++) {
+        if (triggerBefore[i]) triggerCnt++;
+        if (triggerAfter[i]) triggerCnt++;
     }
 }
 
@@ -84,28 +83,36 @@ int main(int argc, char **argv) {
     int clock = 0, cnt = 0, even = 0, oldready = 0, startTime = 0;
     int inPacketTX = 0;
     int cycleStart = 60000;
-    int cycleTime = 13000;  // Do an IN and trigger an OUT each time this cycle
+    int cycleTime = 14000;  // Do an IN and trigger an OUT each time this cycle
     int nextTXTime;
     int nibbles = 0;
     int expected = 64;
-    int nbytesin = 0;
+    int step;
+    unsigned int nbytesin = 0;
+    unsigned int onbytesin = 0;
     XsiStatus status;
     int first = 1;
     int pinLowTime = -1;
     int outputRequired = 0, inputRequired = 0;
-    if (argc != 4) {
-        printf("Usage %s SimArgs toDeviceLen toHostLen (%d)\n", argv[0], argc);
+    int visualise;
+    int packettime, opackettime, oopackettime;
+    if (argc != 6) {
+        printf("Usage %s SimArgs toDeviceLen toHostLen step\n", argv[0]);
         exit(0);
     }
     ltod = atoi(argv[2]);
     ltoh = atoi(argv[3]);
-    setTriggers();
-    fprintf(stdout, "@0LINES %d %d\n", cycleTime/10, triggerCnt);
-    fprintf(stdout, "@0FILE images/blah_%d_%d.ppm\n", ltod, ltoh);
-    fflush(stdout);
+    step = atoi(argv[4]);
+    visualise = atoi(argv[5]);
+    setTriggers(step);
+    if (visualise) {
+        fprintf(stdout, "@0LINES %d %d\n", cycleTime/10, triggerCnt);
+        fprintf(stdout, "@0FILE images/blah_%d_%d.ppm\n", ltod, ltoh);
+        fflush(stdout);
+    }
     status = xsi_create(&xsim, argv[1]);
     assert(status == XSI_STATUS_OK);
-    xsi_write_mem(xsim, "stdcore[0]", 0x1D00C, 4, &ltoh);
+    xsi_write_mem(xsim, "stdcore[0]", 0x1C00C, 4, &ltoh);
     printf("Test %d to host, %d to device\n", ltoh, ltod);
     while (status != XSI_STATUS_DONE) {
         time++;
@@ -118,6 +125,12 @@ int main(int argc, char **argv) {
             outputRequired = 1; // TODO
             inputRequired = 1;
             fprintf(stderr, "%3d\r", triggerCnt);
+/*            if (triggerCnt == 1) {
+                for(int i = 0; i < MT; i++) {
+                    if (triggerBefore[i]) printf("Before %d\n", i);
+                    if (triggerAfter[i]) printf("After %d\n", i);
+                }
+            }*/
         }
         if (outputRequired) {
             if (triggerOutput(time, nextTXTime)) {
@@ -184,14 +197,15 @@ int main(int argc, char **argv) {
                         if (even) {
                             cnt++;
                             if (cnt == 40) {
-                                unsigned ptr, index, len, rdt, wrt;
-                                xsi_read_mem(xsim, "stdcore[0]", 0x1D000, 4, &ptr);
-                                xsi_read_mem(xsim, "stdcore[0]", 0x1D004, 4, &index);
-                                xsi_read_mem(xsim, "stdcore[0]", 0x1D008, 4, &len);
+                                unsigned ptr, index, len, rdt, wrt, ts;
+                                xsi_read_mem(xsim, "stdcore[0]", 0x1C000, 4, &ptr);
+                                xsi_read_mem(xsim, "stdcore[0]", 0x1C004, 4, &index);
+                                xsi_read_mem(xsim, "stdcore[0]", 0x1C008, 4, &len);
+                                xsi_read_mem(xsim, "stdcore[0]", 0x1C018, 4, &ts);
                                 ptr += index;
                                 if (len != ltod) {
                                     if (!first) {
-                                        printf("ERROR: %08x %d\n", ptr, len);
+                                        printf("ERROR: %08x %d\n", ptr-index, len);
                                     }
                                 } else {
                                     if (verbose) {
@@ -200,11 +214,16 @@ int main(int argc, char **argv) {
                                 }
                                 first = 0;
                                 len = 0;
-                                xsi_write_mem(xsim, "stdcore[0]", 0x1D008, 4, &len);
-                                xsi_read_mem(xsim, "stdcore[0]", 0x1D010, 4, &rdt);
-                                xsi_read_mem(xsim, "stdcore[0]", 0x1D014, 4, &wrt);
+                                xsi_write_mem(xsim, "stdcore[0]", 0x1C008, 4, &len);
+                                xsi_write_mem(xsim, "stdcore[0]", 0x1C000, 4, &len);
+                                xsi_write_mem(xsim, "stdcore[0]", 0x1C018, 4, &len);
+                                xsi_read_mem(xsim, "stdcore[0]", 0x1C010, 4, &rdt);
+                                xsi_read_mem(xsim, "stdcore[0]", 0x1C014, 4, &wrt);
                                 if (verbose) {
                                     printf("Diff %d  %d\n", wrt-rdt, rdt*10 - time);
+                                }
+                                if (1 || verbose) {
+                                    printf("Time diff %d   %d %d  %08x\n", startTime/10 - ts, startTime/10, ts, ptr);
                                 }
                             }
                         }
@@ -227,12 +246,17 @@ int main(int argc, char **argv) {
                 xsi_sample_port_pins(xsim, "stdcore[0]", "XS1_PORT_1F", 1, &ready);
                 if (ready) {
                     unsigned nibble;
+                    if (oldready == 0) {
+                        oopackettime = opackettime;
+                        opackettime = packettime;
+                        packettime = time/10;
+                    }
                     xsi_sample_port_pins(xsim, "stdcore[0]", "XS1_PORT_4E", 0xF, &nibble);
                     nibbles++;
                     if (verbose) printf("%01x", nibble);
                     oldready++;
-                    if (oldready >=25 && oldready <=26) {
-                        nbytesin = nbytesin >> 4 | nibble << 4;
+                    if (oldready < 25) {
+                        nbytesin = nbytesin >> 4 | (nibble<<28);
                     }
                 } else {
                     if (oldready) {
@@ -241,7 +265,9 @@ int main(int argc, char **argv) {
                         } else {
                             if (verbose) printf("\n");
                         }
+//                        printf("%d  %d\n", nbytesin, nbytesin - opackettime);
                         fflush(stdout);
+                        onbytesin = nbytesin;
                         nbytesin = 0;
                         nibbles = 0;
                         oldready = 0;
