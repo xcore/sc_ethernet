bdcdaa7a96fd348ea6175aadb996ea421a38552e
Henk Muller <henk@xmos.com>
2012-04-03 18:14:32 +0100

    Skeleton AVB switch

---
+++ app_avb_bridge/src/queue.h
@@ -0,0 +1,7 @@
+// Copyright (c) 2012, XMOS Ltd, All rights reserved
+// This software is freely distributable under a derivative of the
+// University of Illinois/NCSA Open Source License posted in
+// LICENSE.txt and at <http://github.xcore.com/>
+
+void queueManager(chanend qLeft, chanend qRight,
+                  chanend qTransmit, chanend qAVB);
