a4f182dc909fc92909cef6a1548f872bd410cc6b
beaumontm <markb@xmos.com>
2012-11-21 13:26:26 +0000

    Added new board type XP-MC-CTRL-L2

---
+++ module_ethernet_board_support/XP-MC-CTRL-L2/ethernet_board_conf.h
@@ -0,0 +1,7 @@
+#ifndef __ethernet_board_defaults_h__
+#define __ethernet_board_defaults_h__
+
+#define ETHERNET_DEFAULT_TILE tile[0]
+#define ETHERNET_DEFAULT_PHY_ADDRESS 0x0
+
+#endif // __ethernet_board_defaults_h__
