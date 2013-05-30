14ab07741babd7d3e5cd3f1eee5933ea09495b95
Andy Lucas <andy@xmos.com>
2013-05-30 21:29:10 +0100

    Adding board support for D4U module

---
+++ module_ethernet_board_support/D4U-AVB-DG/ethernet_board_conf.h
@@ -0,0 +1,8 @@
+#ifndef __ethernet_board_defaults_h__
+#define __ethernet_board_defaults_h__
+
+#define ETHERNET_DEFAULT_TILE tile[0]
+#define ETHERNET_DEFAULT_PHY_ADDRESS 0x0
+
+
+#endif // __ethernet_board_defaults_h__
