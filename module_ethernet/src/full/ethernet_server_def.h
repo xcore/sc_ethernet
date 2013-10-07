3d49afa9167d0c4d52dc735ec7d3d64efcbe2a1e
Peter Hedinger <peter@xmos.com>
2013-10-07 12:53:53 +0100

    Re-work server def header to use enum instead of #defines - fixing duplicate define.

--- module_ethernet/src/full/ethernet_server_def.h
+++ module_ethernet/src/full/ethernet_server_def.h
@@ -29,49 +29,47 @@
  *
  *****************************************************************************/
 
-// Protocol definitions.
-#define ETHERNET_TX_REQ                   (0x80000000)
-#define ETHERNET_TX_REQ_TIMED             (0x80000001)
-#define ETHERNET_GET_MAC_ADRS             (0x80000002)
-#define ETHERNET_TX_SET_SPACING           (0x80000003)
-#define ETHERNET_TX_REQ_OFFSET2           (0x80000004)
-#define ETHERNET_TX_UPDATE_AVB_ROUTER     (0x80000005)
-#define ETHERNET_TX_INIT_AVB_ROUTER       (0x80000006)
-#define ETHERNET_TX_REQ_HP                (0x80000007)
-#define ETHERNET_TX_REQ_TIMED_HP          (0x80000008)
-#define ETHERNET_TX_REQ_OFFSET2_HP        (0x80000009)
-#define ETHERNET_TX_UPDATE_AVB_FORWARDING (0x80000010)
-
 #define ETHERNET_AVB_ENABLE_FORWARDING      1
 #define ETHERNET_AVB_DISABLE_FORWARDING     0
 
-#if (ETHERNET_TX_HP_QUEUE) && (ETHERNET_TRAFFIC_SHAPER)
-#define ETHERNET_TX_SET_QAV_IDLE_SLOPE   (0x80000010)
-#endif
-
 #define ETHERNET_START_DATA              (0xA5DA1A5A) // Marker for start of data.
 
-#define ETHERNET_RX_FRAME_REQ	         (0x80000010) // Request for ethernet
-                                                      // complete frame,
-                                                      // including src/dest
-#define ETHERNET_RX_TYPE_PAYLOAD_REQ     (0x80000011) // Request for ethernet
-                                                      // type and payload only
-                                                      // (i.e. strip MAC
-                                                      //  address(s))
-#define ETHERNET_RX_OVERFLOW_CNT_REQ     (0x80000012)
-#define ETHERNET_RX_OVERFLOW_MII_CNT_REQ (0x80000013)
-#define ETHERNET_RX_FILTER_SET           (0x80000014)
-#define ETHERNET_RX_DROP_PACKETS_SET     (0x80000015)
-#define ETHERNET_RX_KILL_LINK            (0x80000016)
-#define ETHERNET_RX_CUSTOM_FILTER_SET    (0x80000017)
-#define ETHERNET_RX_QUEUE_SIZE_SET       (0x80000018)
-#define ETHERNET_RX_FRAME_REQ_OFFSET2           (0x80000019)
-#define ETHERNET_RX_WANTS_STATUS_UPDATES_SET    (0x80000020)
-#define ETHERNET_RX_TILE_TIMER_OFFSET_REQ       (0x80000021)
+// Protocol definitions
+typedef enum {
+  ETHERNET_TX_REQ = 0x80000000,
+  ETHERNET_TX_REQ_TIMED,
+  ETHERNET_GET_MAC_ADRS,
+  ETHERNET_TX_SET_SPACING,
+  ETHERNET_TX_REQ_OFFSET2,
+  ETHERNET_TX_UPDATE_AVB_ROUTER,
+  ETHERNET_TX_INIT_AVB_ROUTER,
+  ETHERNET_TX_REQ_HP,
+  ETHERNET_TX_REQ_TIMED_HP,
+  ETHERNET_TX_REQ_OFFSET2_HP,
+  ETHERNET_TX_UPDATE_AVB_FORWARDING,
 
-#define ETHERNET_REQ_ACK	             (0x80000021) // Acknowledged
-#define ETHERNET_REQ_NACK	             (0x80000022) // Negative ack.
+#if (ETHERNET_TX_HP_QUEUE) && (ETHERNET_TRAFFIC_SHAPER)
+  ETHERNET_TX_SET_QAV_IDLE_SLOPE,
+#endif
 
+  ETHERNET_RX_FRAME_REQ,             // Request for ethernet complete frame,
+                                     // including src/dest
+  ETHERNET_RX_TYPE_PAYLOAD_REQ,      // Request for ethernet type and payload only
+                                     // (i.e. strip MAC address(s))
+  ETHERNET_RX_OVERFLOW_CNT_REQ,
+  ETHERNET_RX_OVERFLOW_MII_CNT_REQ,
+  ETHERNET_RX_FILTER_SET,
+  ETHERNET_RX_DROP_PACKETS_SET,
+  ETHERNET_RX_KILL_LINK,
+  ETHERNET_RX_CUSTOM_FILTER_SET,
+  ETHERNET_RX_QUEUE_SIZE_SET,
+  ETHERNET_RX_FRAME_REQ_OFFSET2,
+  ETHERNET_RX_WANTS_STATUS_UPDATES_SET,
+  ETHERNET_RX_TILE_TIMER_OFFSET_REQ,
+
+  ETHERNET_REQ_ACK,
+  ETHERNET_REQ_NACK,
+} ethernet_protocol_t;
 
 #define ETH_BROADCAST (-1)
 
