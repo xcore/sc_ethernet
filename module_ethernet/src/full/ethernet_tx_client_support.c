2184901d2e56ae88bb3ee19fb6d06aadf9172316
Andy Lucas <andy@xmos.com>
2014-10-17 18:14:23 +0100

    Round up on Qav slope calculation.

--- module_ethernet/src/full/ethernet_tx_client_support.c
+++ module_ethernet/src/full/ethernet_tx_client_support.c
@@ -5,7 +5,7 @@ int mac_calc_idle_slope(int bps)
   long long slope;
 
   slope = ((long long) bps) << (MII_CREDIT_FRACTIONAL_BITS);
-  slope = slope / 100000000;
+  slope = (slope + (100000000 - 1)) / 100000000;
 
   return (int) slope;
 }
